04d777f0273107b26274a547bda1793f1d06d297
PhamThiHongNgan <ngannphamm02@gmail.com>
2025-04-19 13:29:26 +0700

    feat/delete-dialog

--- src/page/Home/Test.tsx
+++ src/page/Home/Test.tsx
@@ -6,24 +6,9 @@ import { AccordionItem } from "@radix-ui/react-accordion"
 
 const Test = () => {
     return (
-        <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="outline">Show Dialog</Button>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+       <h1 className="text-3xl font-bold underline">
+            Hello world!
+      </h1>
       )
 }
 
